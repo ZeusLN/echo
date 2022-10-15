5ac8746e273ecf0878dbd4276ad75b157fbce61a
Evan Kaloudis <zeusln@tutanota.com>
2022-10-15 14:23:07 -0400

    Add Zeus branding

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useLNC from '../hooks/useLNC';
+import zeus from '../zeus-logo.svg';
 
 interface Props {
     children?: React.ReactNode;
@@ -17,6 +18,16 @@ const Page: React.FC<Props> = ({ children }) => {
                     <Link to="/" className="navbar-brand">
                         Apollo
                     </Link>
+                    <Navbar.Text>by</Navbar.Text>
+                    <a href="http://zeusln.app" target="_blank">
+                        <img
+                            alt="Zeus"
+                            src={zeus}
+                            width="70"
+                            className="d-inline-block"
+                            style={{ marginLeft: 15 }}
+                        />{' '}
+                    </a>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
